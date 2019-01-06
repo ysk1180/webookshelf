be2231826804ca19f63357d721f818178c93367a
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-06 17:22:34 +0900

    [add]footer追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'terms', to: 'terms#index', as: :terms
+  get 'privacy', to: 'privacies#index', as: :privacy
   root to: 'posts#new'
   get 'posts/show'
   # post 'search', to: 'posts#search'
