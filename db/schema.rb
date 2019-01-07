5c4e1ec28746461804f03791215217ced641214d
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-07 21:32:42 +0900

    [add]twitter_idカラム追加、Twitterへのリンク追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_101801) do
+ActiveRecord::Schema.define(version: 2019_01_07_121429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_01_06_101801) do
     t.string "h"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "twitter_id"
   end
 
 end
