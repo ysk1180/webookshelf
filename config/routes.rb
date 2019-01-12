c124fc2ee4aadf748044bfd398df5c07bf3b3ab4
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-12 21:35:46 +0900

    [add]ランキング機能のベース作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'privacy', to: 'privacies#index', as: :privacy
   root to: 'posts#new'
   get 'bookshelves', to: 'posts#index', as: :bookshelves
+  get 'bookranking', to: 'posts#bookranking', as: :bookranking
   # post 'search', to: 'posts#search'
   post 'make', to: 'posts#make'
 end
