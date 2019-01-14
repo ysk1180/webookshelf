57da02c931b5add17c0017380a2198d660f401b3
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-14 18:33:11 +0900

    [add]本棚一覧・検索にOGP画像設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,6 +51,7 @@ class PostsController < ApplicationController
   end
 
   def index
+    @h = 'bookshelves'
     @q = Post.ransack(params[:q])
     @posts = @q.result.order('created_at desc').page(params[:page]).per(PER)
     @count = Post.all.count
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
       twitter_card[:url] = 'https://webookshelf.herokuapp.com/bookranking'
       twitter_card[:image] = 'https://s3-ap-northeast-1.amazonaws.com/webookshelf-production/ogps/bookranking.png'
       twitter_card[:title] = '本棚に多く入れられた本トップ20'
+    elsif h == 'bookshelves'
+      twitter_card[:url] = 'https://webookshelf.herokuapp.com/bookshelves'
+      twitter_card[:image] = 'https://s3-ap-northeast-1.amazonaws.com/webookshelf-production/ogps/bookshelves.png'
+      twitter_card[:title] = 'あなたの好きな本を検索してみよう。素敵な出会いがあるかも'
     elsif h.present?
       twitter_card[:url] = "https://webookshelf.herokuapp.com/?h=#{h}"
       twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/webookshelf-production/images/#{h}.png"
